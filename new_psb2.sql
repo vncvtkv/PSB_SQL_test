56be009c6d14617de6f33685be6b51f154f629bd
Ivan Tsvetkov <124565814+vncvtkv@users.noreply.github.com>
2025-06-04 20:28:39 +0300

    Add files via upload
    Add some solutions

---
+++ new_psb2.sql
@@ -0,0 +1,89 @@
+-- Вторая задача
+
+-- Создание таблицы credit
+create table if not exists credit (
+credit_id INT primary key,
+issued_date DATE not null
+);
+
+-- Создание таблицы credit_calculations
+create table if not exists credit_calculations (
+credit_id INT,
+calculation_date DATE not null,
+status VARCHAR(10),
+FOREIGN KEY (credit_id) REFERENCES credit (credit_id) ON DELETE cascade,
+CHECK (status IN ('ACTIVE', 'EXPIRED', 'COMPLETED'))
+);
+
+-- Заполнение данными credit
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM credit) THEN
+    INSERT INTO credit (credit_id, issued_date) VALUES 
+(1, '2024-12-31'),
+(2, '2024-12-30'),
+(3, '2024-12-30'),
+(4, '2025-04-05'),
+(5, '2025-05-12'),
+(6, '2025-07-12');
+
+  END IF;
+END $$;
+
+-- Заполнение данными credit_calculations
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM credit_calculations) THEN
+    INSERT INTO credit_calculations (credit_id, calculation_date, status) VALUES 
+(1, '2024-12-31', 'ACTIVE'),
+(1, '2025-01-01', 'ACTIVE'),
+(1, '2025-01-02', 'ACTIVE'),
+(1, '2025-01-03', 'ACTIVE'),
+
+(2, '2024-12-30', 'ACTIVE'),
+(2, '2024-12-31', 'ACTIVE'),
+(2, '2025-01-01', 'EXPIRED'),
+(2, '2025-01-02', 'EXPIRED'),
+
+(3, '2024-12-30', 'ACTIVE'),
+(3, '2024-12-31', 'ACTIVE'),
+(3, '2025-01-01', 'COMPLETED'),
+
+(4, '2025-04-05', 'ACTIVE'),
+(4, '2025-04-06', 'ACTIVE'),
+(4, '2025-04-07', 'EXPIRED'),
+(4, '2025-04-08', 'ACTIVE'),
+(4, '2025-04-09', 'COMPLETED'),
+c
+(5, '2025-05-12', 'ACTIVE'),
+(5, '2025-05-13', 'ACTIVE'),
+
+(6, '2025-07-12', 'ACTIVE'),
+(6, '2025-07-13', 'EXPIRED'),
+(6, '2025-07-14', 'EXPIRED'),
+(6, '2025-07-15', 'EXPIRED'),
+(6, '2025-07-16', 'EXPIRED');
+  END IF;
+END $$;
+
+-- Необходимо::
+-- Первый пункт
+/*
+Для каждого кредита, выданного в этом году, вывести количество дней, когда он
+находился в просрочке (если кредит не имел просрочки, то по таким кредитам выводить
+значение 0)
+*/
+
+select c.credit_id,
+       COUNT(CASE WHEN cc.status = 'EXPIRED' THEN 1 END) AS days_in_expired_status
+from credit c 
+join credit_calculations cc using(credit_id)
+where date_part('year', c.issued_date) = date_part('year', current_date)
+group by c.credit_id
+
+-- Вторая задача
+/*
+для каждого кредита вывести актуальный статус (с максимальной датой calculation_date).
+Здесь хотелось бы увидеть 2 варианта решения – с использованием оконной функции и
+без неё,
+*/
