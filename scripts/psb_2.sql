1b3c952a85937c1adb47d075a4ade4117a090363
vncvtkv <cvetkoviv@yandex.ru>
2025-06-05 19:18:21 +0300

    2nd exercise finished

--- scripts/psb_2.sql
+++ scripts/psb_2.sql
@@ -1,25 +1,25 @@
 -- Вторая задача
 
 -- Создание таблицы credit
-create table if not exists credit (
-credit_id INT primary key,
-issued_date DATE not null
+CREATE TABLE IF NOT EXISTS credit (
+credit_id INT PRIMARY KEY,
+issued_date DATE NOT NULL
 );
 
 -- Создание таблицы credit_calculations
-create table if not exists credit_calculations (
+CREATE TABLE IF NOT EXISTS credit_calculations (
 credit_id INT,
-calculation_date DATE not null,
+calculation_date DATE NOT NULL,
 status VARCHAR(10),
-FOREIGN KEY (credit_id) REFERENCES credit (credit_id) ON DELETE cascade,
-CHECK (status IN ('ACTIVE', 'EXPIRED', 'COMPLETED'))
+FOREIGN KEY (credit_id) REFERENCES credit (credit_id) ON
+DELETE
+	CASCADE,
+	CHECK (status IN ('ACTIVE', 'EXPIRED', 'COMPLETED'))
 );
 
 -- Заполнение данными credit
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM credit) THEN
-    INSERT INTO credit (credit_id, issued_date) VALUES 
+TRUNCATE TABLE credit CASCADE;
+INSERT INTO credit (credit_id, issued_date) VALUES 
 (1, '2024-12-31'),
 (2, '2024-12-30'),
 (3, '2024-12-30'),
@@ -27,17 +27,12 @@ BEGIN
 (5, '2025-05-12'),
 (6, '2025-07-12');
 
-  END IF;
-END $$;
-
 -- Заполнение данными credit_calculations
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM credit_calculations) THEN
-    INSERT INTO credit_calculations (credit_id, calculation_date, status) VALUES 
-(1, '2024-12-31', 'ACTIVE'),
-(1, '2025-01-01', 'ACTIVE'),
-(1, '2025-01-02', 'ACTIVE'),
+TRUNCATE TABLE credit_calculations;
+INSERT INTO credit_calculations (credit_id, calculation_date, status) VALUES 
+(1, '2024-12-31', 'ACTIVE'),    --ACTIVE - активный
+(1, '2025-01-01', 'ACTIVE'),    --EXPIRED - просрочка
+(1, '2025-01-02', 'ACTIVE'),    --COMPLETED - закрытый
 (1, '2025-01-03', 'ACTIVE'),
 
 (2, '2024-12-30', 'ACTIVE'),
@@ -54,7 +49,7 @@ BEGIN
 (4, '2025-04-07', 'EXPIRED'),
 (4, '2025-04-08', 'ACTIVE'),
 (4, '2025-04-09', 'COMPLETED'),
-c
+
 (5, '2025-05-12', 'ACTIVE'),
 (5, '2025-05-13', 'ACTIVE'),
 
@@ -63,44 +58,73 @@ c
 (6, '2025-07-14', 'EXPIRED'),
 (6, '2025-07-15', 'EXPIRED'),
 (6, '2025-07-16', 'EXPIRED');
-  END IF;
-END $$;
 
--- Необходимо::
--- Первый пункт
+
+-- Необходимо:
 /*
-Для каждого кредита, выданного в этом году, вывести количество дней, когда он
+1. Для каждого кредита, выданного в этом году, вывести количество дней, когда он
 находился в просрочке (если кредит не имел просрочки, то по таким кредитам выводить
-значение 0)
+значение 0);
 */
 
-select c.credit_id,
-       COUNT(CASE WHEN cc.status = 'EXPIRED' THEN 1 END) AS days_in_expired_status
-from credit c 
-join credit_calculations cc using(credit_id)
-where date_part('year', c.issued_date) = date_part('year', current_date)
-group by c.credit_id;
+SELECT c.credit_id,
+	   COUNT(CASE WHEN cc.status = 'EXPIRED' THEN 1 END) AS days_in_expired_status
+FROM credit c
+JOIN credit_calculations cc USING(credit_id)
+WHERE date_part('year', c.issued_date) = date_part('year', current_date)
+GROUP BY c.credit_id;
 
--- Второй пункт
+
 /*
-Для каждого кредита вывести актуальный статус (с максимальной датой calculation_date).
-Здесь хотелось бы увидеть 2 варианта решения – с использованием оконной функции и
-без неё,
+2. Для каждого кредита вывести актуальный статус (с максимальной датой calculation_date).
+Написать 2 варианта решения – с использованием оконной функции и без неё;
 */
 
 -- а) без оконной функции
-with max_calc_date as (
-select c.credit_id,
-       MAX(calculation_date) as max_date
-from credit c 
-join credit_calculations cc using(credit_id)
-group by c.credit_id)
+WITH max_calc_date AS (
+SELECT c.credit_id,
+	   MAX(calculation_date) AS max_date
+FROM credit c
+JOIN credit_calculations cc USING(credit_id)
+GROUP BY c.credit_id)
 
-select credit_id,
-       status
-from credit_calculations
-where (credit_id, calculation_date) in (select * from max_calc_date)
-
-
+SELECT credit_id,
+	   status
+FROM credit_calculations
+WHERE (credit_id, calculation_date) IN (SELECT * FROM max_calc_date);
 
 -- б) с оконной функцией
+SELECT DISTINCT
+    credit_id,
+    LAST_VALUE(status) OVER (
+        PARTITION BY credit_id 
+        ORDER BY calculation_date
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+    ) as final_status
+FROM credit_calculations
+ORDER BY credit_id;
+
+/*
+3. После update таблицы credit_calculations , поля
+credit_id и calculation_date остались корректными, а поле status стало частично (null). 
+Требуется написать запрос, показывающий количество кредитов, по которым стали
+пустыми все статусы - вывести количество таких кредитов.
+*/
+
+-- Гипотетический update, занулит все статусы у 4 и 5  credit_id
+UPDATE credit_calculations
+SET status = NULL
+WHERE calculation_date BETWEEN '2025-01-01' AND '2025-06-30';
+
+
+-- Запрос
+SELECT COUNT(DISTINCT cc1.credit_id) AS credit_null_status
+FROM credit_calculations cc1
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM credit_calculations cc2
+    WHERE cc2.credit_id = cc1.credit_id
+    AND cc2.status IS NOT NULL
+);
+
+
